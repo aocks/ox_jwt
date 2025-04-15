5a2d3113f8f977a0726210e142924e25d2368ba3
Emin Martinian <emin.martinian@gmail.com>
2025-04-15 19:24:25 -0400

    cleanup

--- src/ox_jwt/test_app.py
+++ src/ox_jwt/test_app.py
@@ -8,6 +8,7 @@ import socket
 import subprocess
 import sys
 import time
+import typing
 
 from cryptography.hazmat.primitives.asymmetric import ed25519
 from cryptography.hazmat.primitives import serialization
@@ -18,6 +19,8 @@ import requests
 
 
 class FlaskServerManager:
+    """Class to manage flask server.
+    """
 
     process = None
     port = None
@@ -25,7 +28,7 @@ class FlaskServerManager:
     public_key = None
 
     @staticmethod
-    def get_free_port():
+    def get_free_port() -> int:
         "Finds and returns an available port on the system."
         sock = socket.socket()
         sock.bind(('', 0))
@@ -42,53 +45,76 @@ class FlaskServerManager:
         cls.process = None
 
     @classmethod
-    def make_keys(cls, secret_key=None):
-        "Make secret and public key."
-
-        if secret_key is None:
-            secret_key = ed25519.Ed25519PrivateKey.generate()
-    
-        cls.secret_key = base64.b64encode(  # serialize key
-            secret_key.private_bytes(
-                encoding=serialization.Encoding.DER,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption())
-        ).decode('utf8')
-
-        pk = serialization.load_der_private_key(  # de-serialization secret
-            base64.b64decode(cls.secret_key), backend=default_backend(),
-            password=None).public_key()
-        cls.public_key = pk.public_bytes(  # serialize
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ).decode('utf8')
-        
-    @classmethod
     def setup(cls):
+        """Setup server (and keys) and run in subprocess
+        """
         cls.teardown()  # in case process is active
-        cls.make_keys()
+        cls.secret_key, cls.public_key = make_keys()
         cls.port = cls.get_free_port()
         os.chdir(os.path.dirname(__file__))
         env = os.environ.copy()
         env['FLASK_JWT_KEY'] = cls.public_key.split('\n')[1]  # get DER piece
         env['FLASK_JWT_ALGS'] = 'EdDSA,ES256'
-        cls.process = subprocess.Popen([
+        cls.process = subprocess.Popen([  # pylint: disable=consider-using-with
             'flask', 'run', '--port', str(cls.port)], env=env)
-        
-    @classmethod
-    def make_jwt(cls, headers=None, payload=None):
-        sk = serialization.load_der_private_key(  # de-serialize encoded key
-            base64.b64decode(cls.secret_key), backend=default_backend(),
-            password=None)
-        my_jwt = jwt.encode(
-            headers=headers or {'typ':'JWT', 'alg':'EdDSA'},
-            payload=payload or {'sub': 'a', 'name': 'b', 'iat': 1},
-            key=sk)
-        return my_jwt
-        
+
+
+def make_keys(secret_key=None):
+    "Make secret and public key."
+
+    if secret_key is None:
+        secret_key = ed25519.Ed25519PrivateKey.generate()
+
+    serialized_secret_key = base64.b64encode(  # serialize key
+        secret_key.private_bytes(
+            encoding=serialization.Encoding.DER,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption())
+    ).decode('utf8')
+
+    public_key = secret_key.public_key()
+
+    serialized_public_key = public_key.public_bytes(  # serialize
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ).decode('utf8')
+
+    return serialized_secret_key, serialized_public_key
+
+
+def make_jwt(secret_key: str, headers: typing.Optional[dict] = None,
+             payload: typing.Optional[dict] = None) -> str:
+    """Create JWT.
+
+    :param secret_key:   String represening serialized secret key.
+
+    :param headers=None:   Optional dictionary of headers for JWT.
+
+    :param payload=None:   Optional dictionary for payload of JWT.
+
+    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+    :return:  Encoded JWT.
+
+    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+    PURPOSE:  Create JWT from secret key, headers, and payload.
+
+    """
+    sk = serialization.load_der_private_key(  # de-serialize encoded key
+        base64.b64decode(secret_key), backend=default_backend(),
+        password=None)
+    my_jwt = jwt.encode(
+        headers=headers or {'typ': 'JWT', 'alg': 'EdDSA'},
+        payload=payload or {'sub': 'a', 'name': 'b', 'iat': 1},
+        key=sk)
+    return my_jwt
+
 
 @pytest.fixture(scope="session", autouse=True)
 def manage_flask_server_for_tests():
+    "Pytest fixture to setup/teardown flask server for tests."
+
     print('\nSetup FlaskServerManager\n')
     FlaskServerManager.setup()
     time.sleep(1)  # let server get started
@@ -98,35 +124,42 @@ def manage_flask_server_for_tests():
 
 
 def test_simple_enc_dec():
-    my_jwt = FlaskServerManager.make_jwt()
+    """Simple test of encoding/decoding.
+    """
+    my_jwt = make_jwt(FlaskServerManager.secret_key)
     req = requests.get(f'http://127.0.0.1:{FlaskServerManager.port}/hello',
-                       headers={'Authorization': f'Bearer {my_jwt}'})
+                       headers={'Authorization': f'Bearer {my_jwt}'},
+                       timeout=30)
     assert req.status_code == 200
     assert req.text == 'Hello World!'
 
     bad_req = requests.get(f'http://127.0.0.1:{FlaskServerManager.port}/hello',
-                       headers={'Authorization': f'Bearer {my_jwt}mybad'})
+                           headers={'Authorization': f'Bearer {my_jwt}mybad'},
+                           timeout=30)
     assert bad_req.status_code == 401
     assert bad_req.text == (
         "problem=InvalidSignatureError('Signature verification failed')")
 
 
 def test_premium_enc_dec():
+    """Test encoding/decoding with claims for premium user.
+    """
     now = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
-    my_jwt = FlaskServerManager.make_jwt(payload={
+    my_jwt = make_jwt(FlaskServerManager.secret_key, payload={
         'premium_user': 'user@example.com', 'iat': int(now)})
     req = requests.get(
         f'http://127.0.0.1:{FlaskServerManager.port}/support/urgent',
-        headers={'Authorization': f'Bearer {my_jwt}'})
+        headers={'Authorization': f'Bearer {my_jwt}'}, timeout=30)
     assert req.status_code == 200
     assert req.text == 'processing support request for user user@example.com'
 
 
 def main():
+    "Run tests if module is run as main command."
+
     print(f'\n\n   Running tests in {__file__}   \n\n')
     sys.exit(pytest.main([__file__, '-s', '-vvv']))
 
-    
+
 if __name__ == '__main__':
     main()
-    
